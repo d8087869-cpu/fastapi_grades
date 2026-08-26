c99264ba852bc5061cce1124f4f82a44e1122c69
david <d8087869@gmail.com>
2026-08-26 16:32:50 +0300

    complete CRUD student API with client

--- client.py
+++ client.py
@@ -12,30 +12,89 @@ def show_all_students():
 
 
 def find_student():
-    student_id = int(input("Enter student ID: "))
-    response = requests.get(f"{BASE_URL}/students/{student_id}")
+    try:
+        student_id = int(input("Enter student ID: "))
+        response = requests.get(f"{BASE_URL}/students/{student_id}")
+    except ValueError:
+        print("status: 400")
+        print("Invalid input")
+        return
 
     print("Status:", response.status_code)
     print("Response:", response.json())
 
 def add_student():
-    student_id = int(input("Enter student ID: "))
-    name = input("Enter student name: ")
-    grade = int(input("Enter student grade: "))
-
+    try:
+        student_id = int(input("Enter student ID: "))
+        name = input("Enter student name: ")
+        grade = int(input("Enter student grade: "))
+    except ValueError:
+        print("status: 404")
+        print("Inavlid input!!!")
+        return
     student = {
         "student_id": student_id,
         "name": name,
-        "grade": grade
-    }
+        "grade": grade}
 
     response = requests.post(
         f"{BASE_URL}/students",
-        json=student
-    )
+        json=student)
 
     print("Status:", response.status_code)
     print("Response:", response.json())
+
+
+def update_student_grade():
+    try:
+        student_id = int(input("Enter student ID: "))
+    except ValueError:
+        print("status: 404")
+        print("Invalid error")
+        return
+    
+    student = {}
+
+    update_grade = questionary.confirm(
+        "Do you want to update the grade?").ask()
+
+    if update_grade:
+        try:
+            grade = int(input("Enter new grade: "))
+            student["grade"] = grade
+        except ValueError:
+            print("Status: 400")
+            print("Invalid input!!!")
+            return
+
+
+    change_name = questionary.confirm(
+        "Do you want change the name?").ask()
+
+    if change_name:
+        student["name"] = input("Enter new name: ")
+
+    response = requests.put(
+        f"{BASE_URL}/students/{student_id}",
+        json=student)
+
+    print("Status:", response.status_code)
+    print("Response:", response.json())
+
+
+def delete_student():
+    try:
+        student_id = int(input("Enter student ID: "))
+    except ValueError:
+        print("status: 400")
+        print("Invalid input!!!")
+        return
+
+    response = requests.delete(f"{BASE_URL}/students/{student_id}")
+
+    print("Status", response.status_code)
+    print("Response",response.json())
+
 
 def main():
     while True:
@@ -45,11 +104,9 @@ def main():
                 "Show all students",
                 "Find a student",
                 "Add a student",
-                "Update student grade",
+                "Update student grade/name",
                 "Delete a student",
-                "Exit"
-            ]
-        ).ask()
+                "Exit"]).ask()
 
         if choice == "Show all students":
             show_all_students()
@@ -59,6 +116,12 @@ def main():
 
         elif choice == "Add a student":
             add_student()
+
+        elif choice == "Update student grade/name":
+            update_student_grade()
+
+        elif choice == "Delete a student":
+            delete_student()
 
         elif choice == "Exit":
             break
--- src/fastapi_project/main.py
+++ src/fastapi_project/main.py
@@ -2,39 +2,6 @@ from fastapi import FastAPI, HTTPException
 import json
 
 app = FastAPI()
-"""
-@app.get("/")
-def home():
-    return{"message": "Hello world"}
-
-
-@app.get("/hello")
-def hello():
-    return {"message": "Hi FastAPI"}
-
-@app.get("/hello/{name}")
-def hello_name(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.post("/hello")
-def create_hello():
-    return {"message": "Hello was created:)"}
-
-
-@app.delete("/hello")
-def delete_hello():
-    return {"message": "Hello was deleted"}
-
-
-
-# curl localhost:"8000/hello?name=bob"
-# --> bob
-@app.get("/hello")
-def get_name(name):
-    return name
-"""
-
 
 ### students graeds project : 
 
@@ -46,7 +13,7 @@ def load_grades():
 def get_students():
     return load_grades()
 
-
+### GET
 @app.get("/students/{student_id}")
 def get_student(student_id: int):
     students = load_grades()
@@ -57,28 +24,24 @@ def get_student(student_id: int):
 
     raise HTTPException(
         status_code=404,
-        detail="student not found"
-    )
+        detail="student not found")
 
 
 def validate_student_id(student):
     if "student_id" not in student:
         raise HTTPException(
             status_code=400,
-            detail="student_id is must"
-        )
+            detail="student_id is must")
 
     if type(student["student_id"]) is not int:
         raise HTTPException(
             status_code=400,
-            detail="student_id must be an int"
-        )
+            detail="student_id must be an int")
 
     if student["student_id"] <= 0:
         raise HTTPException(
             status_code=400,
-            detail="student_id must be positive"
-        )
+            detail="student_id must be positive")
 
     students = load_grades()
 
@@ -86,31 +49,27 @@ def validate_student_id(student):
         if existing_student["student_id"] == student["student_id"]:
             raise HTTPException(
                 status_code=400,
-                detail="student_id already exists"
-            )
+                detail="student_id already exists")
 
 def validate_name(student):
     if not (
         "name" in student
         and type(student["name"]) is str
-        and student["name"] != ""
-    ):
+        and student["name"] != ""):
         raise HTTPException(
             status_code=400,
-            detail="Invalid input, please check your input"
-        )
+            detail="Invalid input, please check your input")
 
 def validate_grade(student):
     if not (
         "grade" in student
         and type(student["grade"]) is int
-        and 0 <= student["grade"] <= 100
-    ):
+        and 0 <= student["grade"] <= 100):
         raise HTTPException(
             status_code=400,
-            detail="Invalid input, please check your input"
-        )
-
+            detail="Invalid input, please check your input")
+        
+### POST
 @app.post("/students")
 def add_student(student: dict):
     validate_student_id(student)
@@ -126,3 +85,53 @@ def add_student(student: dict):
 
     return student         
 
+### PUT
+@app.put("/students/{student_id}")
+def update_student_grade(student_id: int, student: dict):
+    validate_grade(student)
+    students = load_grades()
+    found = False
+
+    for student_data in students:
+        if student_data["student_id"] == student_id:
+
+            if "grade" in student:
+                validate_grade(student)
+                student_data["grade"] = student["grade"]
+
+            if "name" in student:
+                validate_name(student)
+                student_data["name"] = student["name"]
+            
+            found = True
+
+            
+    if not found:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found")
+
+
+    with open("grades.json", "w") as file:
+        json.dump(students, file)
+
+    return student
+
+### DELETE
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    students = load_grades()
+
+    for student in students:
+        if student["student_id"] == student_id:
+            students.remove(student)
+
+            with open("grades.json", "w") as file:
+                json.dump(students, file)
+
+            return student
+    raise HTTPException(
+        status_code=404,
+
+        detail="student not found"
+    )
